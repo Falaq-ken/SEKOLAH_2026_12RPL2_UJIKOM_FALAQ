5f3c04dd3a01d790bb04c8bfbe79fc32d37e4986
Falaq <152266492+Falaq-ken@users.noreply.github.com>
2026-01-14 17:07:09 +0700

    Update cari-pengaduan.php

---
+++ UJIKOMFALAQ/cari-pengaduan.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cari pengaduan</title>
+</head>
+<body>
+    Halaman cari pengaduan
+
+    <form action="">
+        <div>
+<label for="">Nis :</label> <br>
+<input type="text">
+</div>
+
+<div>
+<label for="">lokasi :</label> <br>
+<input type="text">
+</div>
+
+<div>
+<label for="">Keterangan</label> <br>
+<textarea></textarea>
+</div>
+
+<button>kirim</button>
+
+    </form>
+</body>
+</html>
