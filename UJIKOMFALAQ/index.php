15faa94e42b4d03d3d8552797c49c8f808ee00ff
Falaq <152266492+Falaq-ken@users.noreply.github.com>
2026-01-15 16:33:39 +0700

    Revamp index.php layout and styles

--- UJIKOMFALAQ/index.php
+++ UJIKOMFALAQ/index.php
@@ -3,16 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <title>PENGADUAN MUTU</title>
+<style>
+
+    body{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        height: 100vh;
+        background-color: rgb(105, 182, 255);
+        font-family: monospace;
+
+    }
+
+    .button{
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+    }
+
+    .button1{
+         background-color: rgb(86, 255, 142);
+        border: none;
+        padding: 5px;
+        border: 1px solid;
+        border-radius: 5px;
+         font-family: monospace;
+    }
+
+    .button2{
+        background-color: rgb(105, 182, 255);
+        border: none;
+        padding: 5px;
+        border: 1px solid;
+        border-radius: 5px;
+        font-family: monospace;
+        
+    }
+
+    .isi{
+        background-color: white;
+        padding: 30px;
+        border-radius: 5px;
+        text-align: center;
+        border-color: black;
+        border: 1px solid;
+        border-radius: 5px;
+    }
+
+
+</style>
 </head>
-<body>
-<div>
-    <h3>Selamat datang di website pengaduan mutu</h3>
-    <a href="form-pengaduan.php">buat pengaduan</a>
-    <a href="cari-pengaduan.php">cari Pengaduan</a>
+<body> 
+
+<div class="isi"> 
+     <h1>SELAMAT DATANG DI</h1>
+     <h3>website pengaduan mutu</h3>
+<div class="button">
+   
+    <a href="form-pengaduan.php"> <button class="button1">Buat pengaduan</button></a>
+    <a href="cari-pengaduan.php"> <button class="button2">Cari Pengaduan</button></a>
 
     
 </div>
-
+</div>
 </body>
 </html>
 
