ce73c04e1bafa5c8d81992cd47cd2489ebdb9da0
Falaq <152266492+Falaq-ken@users.noreply.github.com>
2026-01-15 16:33:14 +0700

    Add login.php with basic HTML structure

---
+++ UJIKOMFALAQ/login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <style>
+
+        body{
+            font-family: monospace;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            margin: 0;
+            height: 90vh;
+            background-color: rgb(105, 182, 255);
+        }
+
+        .box{
+            background-color: white;
+            padding: 25px;
+            border: 1px solid;
+            border-radius: 5px;
+        }
+        
+        .button{
+            border: none;
+            border: 1px solid;
+            padding: 5px;
+            background-color: rgb(105, 182, 255);
+            border-radius: 5px;
+            font-family: monospace;
+
+        }
+    </style>
+</head>
+<body>
+    
+
+
+
+
+<div class="box">
+<form action="">
+   <b>LOGIN</b><br>
+    <br>
+    <label for="">Username :</label><br>
+    <input type="text"> <br>
+
+    <label for="">password :</label><br>
+    <input type="text"> <br>
+    
+   
+
+</form>
+    <br><a href="index.php"> <button class="button">Login</button></a>
+</div>
+
+</body>
+</html>
