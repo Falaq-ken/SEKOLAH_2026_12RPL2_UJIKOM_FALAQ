58a109469a7fd9805819355f5d12e1bddd6ff6e8
Falaq <152266492+Falaq-ken@users.noreply.github.com>
2026-01-15 16:34:26 +0700

    Enhance layout and style for cari pengaduan page
    Added styling and improved layout for the search complaints page.

--- UJIKOMFALAQ/cari-pengaduan.php
+++ UJIKOMFALAQ/cari-pengaduan.php
@@ -4,28 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cari pengaduan</title>
+
+    <style>
+
+        body{
+            font-family: monospace;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            margin: 0;
+            height: 90vh;
+            background-color: rgb(105, 182, 255);
+        }
+
+
+             
+
+        .box{
+            background-color: white;
+            padding: 25px;
+            border: 1px solid;
+            border-radius: 5px;
+        }
+        
+        .button{
+            border: none;
+            border: 1px solid;
+            padding: 5px;
+            background-color: rgb(105, 182, 255);
+            border-radius: 5px;
+            font-family: monospace;
+        }
+            
+
+            
+    </style>
 </head>
 <body>
-    Halaman cari pengaduan
+    
 
-    <form action="">
-        <div>
-<label for="">Nis :</label> <br>
-<input type="text">
+<div class="box">
+<form action="">
+   <b>CARI PENGADUAN </b>  <br>
+    <br>
+    <label for=""></label><br>
+    <input type="text"> <br>
+
+</form>
+    <br><button class="button">cari</button>
+    <a href="index.php"> <button class="button">Ga jadi</button></a>
+
 </div>
 
-<div>
-<label for="">lokasi :</label> <br>
-<input type="text">
-</div>
-
-<div>
-<label for="">Keterangan</label> <br>
-<textarea></textarea>
-</div>
-
-<button>kirim</button>
-
-    </form>
 </body>
 </html>
