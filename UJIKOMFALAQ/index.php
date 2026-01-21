aabbc653821a91794b33851ff48b81e0f0cc4a3a
Falaq <152266492+Falaq-ken@users.noreply.github.com>
2026-01-21 16:55:32 +0700

    Update index.php

--- UJIKOMFALAQ/index.php
+++ UJIKOMFALAQ/index.php
@@ -6,49 +6,52 @@
 <style>
 
     body{
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-        height: 100vh;
-        background-color: rgb(105, 182, 255);
+        margin: 0;
+        padding: 0;
         font-family: monospace;
+    }
 
+    .navbar {
+        width: 100%;
+        height: 50px;
+        background-color: rgb(105, 182, 255);
+        margin-bottom: 20px;
+    }
+
+    .isidiv{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 95%;
+        margin: 0 auto;
+        font-family: 'Baloo 2';
+        color: black;
     }
 
     .button{
-        display: flex;
-        justify-content: center;
-        gap: 20px;
-    }
-
-    .button1{
-         background-color: rgb(86, 255, 142);
-        border: none;
-        padding: 5px;
-        border: 1px solid;
-        border-radius: 5px;
-         font-family: monospace;
-    }
-
-    .button2{
-        background-color: rgb(105, 182, 255);
-        border: none;
-        padding: 5px;
-        border: 1px solid;
-        border-radius: 5px;
-        font-family: monospace;
-        
-    }
+         border: none;
+            border: 1px solid;
+            padding: 5px;
+            background-color: rgb(105, 182, 255);
+            border-radius: 7px;
+            font-family: monospace;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            width: 150px;
+            height: 35px;
+            padding: 0 12px;
+            border-radius: 10px;
+            border: 1px solid black;
+            font-size: 14px;
+            box-sizing: border-box;
+        }
 
     .isi{
-        background-color: white;
+        margin-left: 5px;
         padding: 30px;
-        border-radius: 5px;
-        text-align: center;
-        border-color: black;
-        border: 1px solid;
-        border-radius: 5px;
+        text-align: start;
+        width: 300px;
+        font-family: 'Baloo 2';
     }
 
 
@@ -56,17 +59,25 @@
 </head>
 <body> 
 
-<div class="isi"> 
-     <h1>SELAMAT DATANG DI</h1>
-     <h3>website pengaduan mutu</h3>
-<div class="button">
-   
-    <a href="form-pengaduan.php"> <button class="button1">Buat pengaduan</button></a>
-    <a href="cari-pengaduan.php"> <button class="button2">Cari Pengaduan</button></a>
+<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@500;600;700;800&display=swap" rel="stylesheet">
 
-    
+<nav class="navbar"> 
+    <div class="isidiv">
+    <h1 style="color: black; margin: 0;">PENGADUAN MUTU</h1>
+    <a href="login.php"> <button class="button">LOGIN</button></a> 
+    </div>
+</nav>
+
+<div class="isi"> 
+     <h1><b>Selamat datang di!</b></h1>
+      <h1><b>Pengaduan MUTU</b></h1>
+      <h3><b>silahkan pilih ingin buat pengaduan atau hanya ingin cari</b></h3>
+<div>
+<a href="form-pengaduan.php"> <button class="button">BUAT PENGADUAN</button></a> 
+  <a href="cari-pengaduan.php"> <button class="button">CARI PENGADUAN</button></a>
 </div>
 </div>
+
 </body>
 </html>
 
