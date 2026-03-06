ebd6e70f3e288b12db9f8aac4d28c03486718feb
Falaq <152266492+Falaq-ken@users.noreply.github.com>
2026-03-06 22:42:52 +0700

    Delete UJIKOMFALAQ directory

--- UJIKOMFALAQ/admin/hapus-kategori.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_falaq");
-
-$id = $_GET['id'];
-
-mysqli_query($koneksi, "DELETE FROM kategori WHERE id_kategori='$id'");
-
-header("Location: kategori.php");
-?>  
--- UJIKOMFALAQ/admin/hapus-siswa.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_falaq");
-
-$id = $_GET['id'];
-
-mysqli_query($koneksi, "DELETE FROM user WHERE id='$id'");
-
-header("Location: data-siswa.php");
-?>
